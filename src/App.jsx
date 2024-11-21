506e674a4e78741c108d19d7a9869f3258dc4601
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2024-11-21 20:27:51 -0300

    feat: Search Section HTML/JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  return <div className="container">
+    
+    {/* Search Section*/}
+    <div className="search-section">
+      <form action="#" className="search-form">
+        <span className="googlefonts">search</span>
+        <input type="search" placeholder="Enter a city name" 
+        className="search-input" required/>
+      </form>
+      <button className="location-button">
+        <span className="googlefonts">my_location</span>
+      </button>
+    </div>
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    {/* */}
+  </div>
 }
 
 export default App
